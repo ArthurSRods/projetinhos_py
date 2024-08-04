c9dcb73cab02e528fb9f741d16576bbe8682aab5
ArthurSRods <arthur.srodrigues@hotmail.com>
2024-08-04 17:02:07 -0300

    Defini uma função

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,13 +1,16 @@
 #CALCULO DO IMC
-nome=input('Qual o seu nome? ')
-idade=input(f'{nome} qual sua idade? ')
-peso= None
-while peso is None:
-    try: peso = float(input('Quanto você pesa? '))
-    except ValueError: ('Tente novamente.')
-altura= None
-while altura is None:
-    try: altura = float(input('Qual sua altura? '))
-    except ValueError: ('Tente novamente.')
-imc= peso/(altura)**2
-print(f'{nome} seu IMC é de {imc:.2f}')
+def calculadora_imc():
+    nome=input('Qual o seu nome? ')
+    idade=input(f'{nome} qual sua idade? ')
+    peso= None
+    while peso is None:
+        try: peso = float(input('Quanto você pesa? '))
+        except ValueError: ('Tente novamente.')
+    altura= None
+    while altura is None:
+        try: altura = float(input('Qual sua altura? '))
+        except ValueError: ('Tente novamente.')
+    imc= peso/(altura)**2
+    return (f'{nome} seu IMC é de {imc:.2f}')
+
+print(calculadora_imc())
